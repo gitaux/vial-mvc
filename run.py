aa7ee06db09860487f48ccb0aacd55c5806734f3
aux0 <mcr@tuta.io>
2017-09-27 20:08:40 +0200

    Version & License header added.

--- run.py
+++ run.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
 # run.py
+#
+# License:   MIT License
+# Author:    Roland Lucien Maxand
+# Email:     r.maxand@outlook.com
+# Github:    https://github.com/gitaux
+# Copyright: (c) 2017 Roland Lucien Maxand
 
 import os
 
 from app import create_app
+
+
+__version__ = '0.1.0'
 
 config_name = os.getenv('FLASK_CONFIG')
 app = create_app(config_name='development')
